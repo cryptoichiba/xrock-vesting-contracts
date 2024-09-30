a2e4d519cdf7bebdfeec6bbf804d748acd1f2056
rudo <rudoneko@gmail.com>
2024-09-30 17:00:21 +0900

    claimerとdeployerのmnemonicに対応

--- scripts/deployJettonLockup.ts
+++ scripts/deployJettonLockup.ts
@@ -1,22 +1,41 @@
-import { Address, beginCell, toNano, Cell, storeStateInit } from '@ton/core';
+import { OpenedContract, Sender, SendMode, toNano } from '@ton/core';
 import { JettonLockup, VestingData } from '../wrappers/JettonLockup';
 import { compile, NetworkProvider } from '@ton/blueprint';
-import { randomAddress } from '@ton/test-utils';
 
-import { mnemonicToPrivateKey } from "@ton/crypto";
-import { WalletContractV5R1, JettonMaster } from "@ton/ton";
-import { JETTON_MASTER_ADDRESS, JETTON_WALLET_CODE } from "../wrappers/Config"
+import { KeyPair, mnemonicToPrivateKey } from '@ton/crypto';
+import { WalletContractV5R1, JettonMaster, internal } from "@ton/ton";
+import { JETTON_MASTER_ADDRESS } from "../wrappers/Config"
+
+async function createWallet(provider: NetworkProvider, mnemonic: string) {
+    const keys = await mnemonicToPrivateKey(mnemonic.split(" "));
+    const wallet = provider.open(
+        WalletContractV5R1.create({
+            workchain: 0,
+            publicKey: keys.publicKey,
+        }),
+    );
+    return [keys, wallet, wallet.sender(keys.secretKey)];
+}
 
 export async function run(provider: NetworkProvider) {
-
-
     const tokenBalanceConfig = 25000n;
-    let claimerAddress = Address.parse("UQDu4Pma1lCVJtlBZV0_ImCBXTNbkgodGrdUU3jlBV0_qQZG")
+
+    let deployerKeys: KeyPair;
+    let deployerWallet: OpenedContract<WalletContractV5R1>;
+    let deployerSender: Sender;
+    // @ts-ignore
+    [deployerKeys, deployerWallet, deployerSender] = await createWallet(provider, process.env.WALLET_MNEMONIC);
+
+    let claimerKeys: KeyPair;
+    let claimerWallet: OpenedContract<WalletContractV5R1>;
+    let claimerSender: Sender;
+    // @ts-ignore
+    [claimerKeys, claimerWallet, claimerSender] = await createWallet(provider, process.env.WALLET_CLAIM_MNEMONIC);
 
     const jettonLockup = provider.open(
         JettonLockup.createFromConfig({
-            adminAddress: provider.sender().address!,
-            claimerAddress: claimerAddress
+            adminAddress: deployerSender.address!,
+            claimerAddress: claimerSender.address!
         }, await compile('JettonLockup')));
 
     const jettonMaster = provider.open(JettonMaster.create(JETTON_MASTER_ADDRESS));
@@ -47,22 +66,26 @@ export async function run(provider: NetworkProvider) {
 
     await provider.waitForDeploy(jettonLockup.address);
 
+    /** TODO: ここでjettonを送る
+    const seqno = await deployerWallet.getSeqno();
+    await deployerWallet.sendTransfer({
+        seqno,
+        secretKey: deployerKeys.secretKey,
+        sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
+        messages: [
+            internal({
+                value: '0.001',
+                to: lockup_contract_jetton_wallet_address,
+                body: 'Hello world'
+            })
+        ]
+    });
+    **/
+
     const claimableTokens = await jettonLockup.getClaimableTokens();
     console.log('Claimable Tokens: ', claimableTokens);
 
     const minFee = await jettonLockup.getMinFee();
-
-    // @ts-ignore
-    const mnemonic = process.env.WALLET_MNEMONIC.split(" ");
-
-    const keys = await mnemonicToPrivateKey(mnemonic);
-    const wallet = provider.open(
-      WalletContractV5R1.create({
-          workchain: 0,
-          publicKey: keys.publicKey,
-      }),
-    );
-    const claimerSender = wallet.sender(keys.secretKey);
 
     await jettonLockup.sendClaimTokens(claimerSender, minFee, tokenBalanceConfig);
 }
